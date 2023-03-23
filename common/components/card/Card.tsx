8907f4120e4868f932d9f070e37fd03b2872d304
nairpa <nairpalacios@live.com>
2023-03-23 18:50:53 -0300

    feat: adds card component

--- common/components/card/Card.tsx
+++ common/components/card/Card.tsx
@@ -1,16 +1,38 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useState } from 'react';
 import styles from './Card.module.css';
 
 export const Card = ({children}: PropsWithChildren) => {
-    return (
-        <div className={styles.cardContainer}>
-            <div className={styles.cardButtons}>
-                <button>example</button>
-                <button>code</button>
-            </div>
-            <div className={styles.card}>
+    const [ tab, setTab ] = useState('example');
+
+    const handleTabChange = (e: any) => {
+        setTab(e.target.id);
+    }
+
+    const getCurrentTab = () => {
+        if(tab === 'example') {
+            return (
+            <div className={styles.cardContainer}>
+                <div className={styles.card}>
                 {children}
+                </div>
+            </div>)
+        } else if(tab === 'code') {
+            return (
+                <div className={styles.cardCode}>
+                    <div className={styles.card}>
+                        {`<Button color="primary" variant="text">`}
+                    </div>
+                </div>
+            )
+        }
+    }
+    return (
+        <div className={styles.container}>
+            <div className={styles.cardButtons}>
+                <button name='example' id="example" onClick={(e) => handleTabChange(e)}>example</button>
+                <button name='code' id="code" onClick={(e) => handleTabChange(e)}>code</button>
             </div>
+            { getCurrentTab() }
         </div>
     )
 }
